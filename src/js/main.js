da70028f8d96054497420b4c65e778dcb6b53ddd
Semdev1986 <arsentver@gmail.com>
2020-11-02 16:33:25 +0300

    update deps and any js files

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/extensions
 import summ from './modules/summ.js';
 
-summ(2, 8);
-
+console.log(summ(2, 8));
+console.log(summ(3, 5));
 document.addEventListener('click', ({ target }) => {
   console.log(target);
 });
--- src/js/modules/summ.js
+++ src/js/modules/summ.js
@@ -1,4 +1,1 @@
-export default function (a, b) {
-  console.log(a + b);
-  return a + b;
-}
+export default (a, b) => a + b;
